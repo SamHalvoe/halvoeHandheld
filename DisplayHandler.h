ade2cc98d301b63de769bc1d698ed3576338ed58
SamHalvoe <46531812+SamHalvoe@users.noreply.github.com>
2024-03-24 16:31:48 +0100

    initial working piece

--- DisplayHandler.h
+++ DisplayHandler.h
@@ -1,26 +1,34 @@
 #pragma once
 
+#include <array>
+
 #include <Adafruit_GFX.h>
-#include <Adafruit_HX8357.h>
+#include <HX8357_t3n.h>
 #include <Adafruit_FT5336.h>
 
 const uint8_t TFT_BACKLIGHT_PIN = 9;
 const uint8_t TFT_RESET_PIN = 7;
-const uint8_t TFT_DC_PIN = 8;
+const uint8_t TFT_DC_PIN = 38;
 const uint8_t TFT_CS_PIN = 0;
 const uint8_t TFT_MOSI_PIN = 26;
 const uint8_t TFT_MISO_PIN = 1;
 const uint8_t TFT_SCK_PIN = 27;
-const uint32_t TFT_SPI_FREQ = 24000000;
+const uint32_t TFT_SPI_FREQ = 30000000;
 const uint8_t TOUCH_SCL_PIN = 16;
 const uint8_t TOUCH_SDA_PIN = 17;
-const uint16_t displayWidth = 480;
-const uint16_t displayHeight = 320;
+const uint16_t displayWidth = 320;
+const uint16_t displayHeight = 480;
+const uint32_t displayPixelNumber = displayWidth * displayHeight;
 
 const uint8_t touchDeviceMaxTouches = 5;
 
-Adafruit_HX8357 displayDevice(&SPI1, TFT_CS_PIN, TFT_DC_PIN, TFT_RESET_PIN, HX8357D);
-//Adafruit_HX8357 displayDevice(TFT_CS_PIN, TFT_DC_PIN, TFT_MOSI_PIN, TFT_SCK_PIN, TFT_RESET_PIN, TFT_MISO_PIN, HX8357D);
+HX8357_t3n displayDevice(TFT_CS_PIN, TFT_DC_PIN, TFT_RESET_PIN, TFT_MOSI_PIN, TFT_SCK_PIN, TFT_MISO_PIN);
+DMAMEM uint16_t displayFrameBuffer[displayPixelNumber];
+
+std::array<uint16_t, 4> displayPalette = { HX8357_BLACK, HX8357_RED, HX8357_GREEN, HX8357_BLUE };
+
+GFXcanvas16 rectangle(200, 300);
+
 Adafruit_FT5336 touchDevice;
 
 void setupDisplay()
@@ -29,27 +37,41 @@ void setupDisplay()
 
   analogWrite(TFT_BACKLIGHT_PIN, 255);
 
-  delay(5000);
+  delay(2000);
   displayDevice.begin(TFT_SPI_FREQ);
-  //touchDevice.begin(FT53XX_DEFAULT_ADDR, &Wire1);
+  displayDevice.setFrameBuffer(displayFrameBuffer);
+  displayDevice.updateChangedAreasOnly(true);
+
+  displayDevice.fillScreen(HX8357_BLACK);
+  displayDevice.updateScreen();
 
   Serial.println("-- Display Infos Begin --");
 
-  uint8_t x = displayDevice.readcommand8(HX8357_RDPOWMODE);
-  Serial.print("Display Power Mode: 0x"); Serial.println(x, HEX);
-  x = displayDevice.readcommand8(HX8357_RDMADCTL);
-  Serial.print("MADCTL Mode: 0x"); Serial.println(x, HEX);
-  x = displayDevice.readcommand8(HX8357_RDCOLMOD);
-  Serial.print("Pixel Format: 0x"); Serial.println(x, HEX);
-  x = displayDevice.readcommand8(HX8357_RDDIM);
-  Serial.print("Image Format: 0x"); Serial.println(x, HEX);
-  x = displayDevice.readcommand8(HX8357_RDDSDR);
-  Serial.print("Self Diagnostic: 0x"); Serial.println(x, HEX); 
+  uint8_t infoValue = displayDevice.readcommand8(HX8357_RDMODE);
+  Serial.print("Display Power Mode: 0x"); Serial.println(infoValue, HEX);
+  infoValue = displayDevice.readcommand8(HX8357_RDMADCTL);
+  Serial.print("MADCTL Mode: 0x"); Serial.println(infoValue, HEX);
+  infoValue = displayDevice.readcommand8(HX8357_RDPIXFMT);
+  Serial.print("Pixel Format: 0x"); Serial.println(infoValue, HEX);
+  infoValue = displayDevice.readcommand8(HX8357_RDIMGFMT);
+  Serial.print("Image Format: 0x"); Serial.println(infoValue, HEX);
+  infoValue = displayDevice.readcommand8(HX8357_RDSELFDIAG);
+  Serial.print("Self Diagnostic: 0x"); Serial.println(infoValue, HEX);
 
   Serial.println("-- Display Infos End --");
 
-  displayDevice.fillScreen(HX8357_GREEN);
-  //displayDevice.fillRect(0, 0, 64, 64, HX8357_GREEN);
+  //touchDevice.begin(FT53XX_DEFAULT_ADDR, &Wire1);
+
+  elapsedMillis timeFullUpdate;
+
+  for (uint8_t index = 0; index < 4; ++index)
+  {
+    timeFullUpdate = 0;
+    rectangle.fillScreen(displayPalette.at(index));
+    displayDevice.writeRect(0, 0, rectangle.width(), rectangle.height(), rectangle.getBuffer());
+    displayDevice.updateScreen();
+    Serial.println("timeUpdateScreen: " + String(timeFullUpdate) + " ms");
+  }
 
   Serial.println("---- Display Setup End ----");
 }
