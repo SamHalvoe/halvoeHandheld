49194e6d7cd5a68c9560de8089d90fb5c141ff59
SamHalvoe <46531812+SamHalvoe@users.noreply.github.com>
2024-10-20 19:36:56 +0200

    WIP: SerialAudioInterface

--- SerialAudioController.hpp
+++ SerialAudioController.hpp
@@ -9,49 +9,45 @@ namespace halvoe
 
 	class SerialAudioController : public SerialInterface<c_serializerBufferSize, c_deserializerBufferSize>
 	{
-	public:
-		SerialAudioController(HardwareSerial& in_serial) : SerialInterface<c_serializerBufferSize, c_deserializerBufferSize>(in_serial)
-		{}
+		public:
+			SerialAudioController(HardwareSerial& in_serial) : SerialInterface<c_serializerBufferSize, c_deserializerBufferSize>(in_serial)
+			{}
 
-		bool setup()
-		{
-			m_serial.begin(19200);
+			bool setup()
+			{
+				m_serial.begin(19200);
 
-			return m_serial;
-		}
+				return m_serial;
+			}
 
-		bool playFile(const String& in_filename)
-		{
-			Serial.println("playFile()");
+			bool playFile(const String& in_filename)
+			{
+				Serial.println("playFile()");
 
-			auto message = beginMessage(SerialMessageType::command);
-			message.m_serializer.writeEnum<SerialCommandCode>(SerialCommandCode::playFile);
-			message.m_serializer.write<SerialStringSizeType>(in_filename.c_str(), in_filename.length());
+				auto message = beginMessage(SerialMessageType::command);
+				message.m_serializer.writeEnum<SerialCommandCode>(SerialCommandCode::playFile);
+				message.m_serializer.write<SerialStringSizeType>(in_filename.c_str(), in_filename.length());
+				return sendMessage(message);
+			}
 
-			size_t bytesWritten = message.m_serializer.getBytesWritten();
-			return sendMessage(message) == bytesWritten;
-		}
+			bool endPlayback()
+			{
+				Serial.println("endPlayback()");
 
-		bool endPlayback()
-		{
-			Serial.println("endPlayback()");
+				auto message = beginMessage(SerialMessageType::command);
+				message.m_serializer.writeEnum<SerialCommandCode>(SerialCommandCode::endPlayback);
+				return sendMessage(message);
+			}
 
-			auto message = beginMessage(SerialMessageType::command);
-			message.m_serializer.writeEnum<SerialCommandCode>(SerialCommandCode::endPlayback);
+		private:
+			bool doHandleData(Deserializer<c_deserializerBufferSize>& io_deserializer, SerialDataCode in_code)
+			{
+				return false;
+			}
 
-			size_t bytesWritten = message.m_serializer.getBytesWritten();
-			return sendMessage(message) == bytesWritten;
-		}
-
-	private:
-		bool doHandleData(Deserializer<c_deserializerBufferSize>&& in_deserializer, SerialDataCode in_code)
-		{
-			return false;
-		}
-
-		bool doHandleCommand(Deserializer<c_deserializerBufferSize>&& in_deserializer, SerialCommandCode in_code)
-		{
-			return false;
-		}
+			bool doHandleCommand(Deserializer<c_deserializerBufferSize>& io_deserializer, SerialCommandCode in_code)
+			{
+				return false;
+			}
 	};
 }
